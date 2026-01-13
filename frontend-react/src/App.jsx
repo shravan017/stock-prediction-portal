76b22ab75c2408fbebc6d977a4b922ce18a38ce8
Shravan017 <shravannaik027@gmail.com>
2026-01-14 00:18:22 +0530

    created rigistration form

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,16 +1,25 @@
 import { useState } from 'react'
 import './assets/css/style.css'
 import Main from './components/Main'
+import Register from './components/Register'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Footer from './components/Footer'
 import Header from './components/Header'
+import Login from './components/Login'
 
 function App() {
 
   return (
     <>
+      <BrowserRouter>
       <Header />
-      <Main />
+        <Routes>
+          <Route path='/' element={<Main />} />
+          <Route path='/register' element={<Register/>} />
+          <Route path='/login' element={<Login/>}/>
+        </Routes>
       <Footer />
+      </BrowserRouter>
     </>
   )
 }
--- frontend-react/src/components/Header.jsx
+++ frontend-react/src/components/Header.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import Button from './Button'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <>
         <nav className='navbar container pt-4 pb-4 align-items-start' >
-            <b><a className='navbar-brand text-light' href=''>STOCK PREDICTION PORTAL</a></b>
+            <b><Link className='navbar-brand text-light' to='/'>STOCK PREDICTION PORTAL</Link></b>
             <div>
-                <Button text="Login" class="btn-outline-info"/>
+                <Button text="Login" class="btn-outline-info" url="login"/>
                 &nbsp;
-                <Button text="Register" class="btn-info"/>
+                <Button text="Register" class="btn-info" url="register"/>
             </div>
         </nav>
     </>
