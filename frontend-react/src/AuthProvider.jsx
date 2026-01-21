cb9dae9ca8d819e91668e917d1f52eac67cad671
Shravan017 <shravannaik027@gmail.com>
2026-01-22 00:14:45 +0530

    Login functionality with authentication

---
+++ frontend-react/src/AuthProvider.jsx
@@ -0,0 +1,19 @@
+import React, { useState, useContext, createContext } from 'react'
+
+const AuthContext = createContext();
+const AuthProvider = ( {children} ) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(
+    //check true or false
+    !!localStorage.getItem('accessToken')
+  )
+  return (
+    <AuthContext.Provider value={{isLoggedIn ,setIsLoggedIn}}>
+      { children }
+    </AuthContext.Provider>
+    
+  )
+}
+
+export default AuthProvider
+
+export { AuthContext }
--- frontend-react/src/components/Header.jsx
+++ frontend-react/src/components/Header.jsx
@@ -1,16 +1,39 @@
-import React from 'react'
+import { useContext } from 'react'
 import Button from './Button'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { AuthContext } from '../AuthProvider'
 
 const Header = () => {
+  const { isLoggedIn, setIsLoggedIn } = useContext(AuthContext)
+  const navigate = useNavigate();
+  
+  const handleLogout = (e) => {
+
+    localStorage.removeItem('accessToken')
+    localStorage.removeItem('refreshToken')
+    setIsLoggedIn(false)
+    console.log('Logged out')
+    navigate('/login')
+
+  }
   return (
     <>
         <nav className='navbar container pt-4 pb-4 align-items-start' >
             <b><Link className='navbar-brand text-light' to='/'>STOCK PREDICTION PORTAL</Link></b>
+
+            
             <div>
-                <Button text="Login" class="btn-outline-info" url="login"/>
-                &nbsp;
-                <Button text="Register" class="btn-info" url="register"/>
+              {isLoggedIn? (
+                <button className='btn btn-danger' onClick={handleLogout}>Logout</button>
+              ):(
+                <>
+                  <Button text="Login" class="btn-outline-info" url="login"/>
+                  &nbsp;
+                  <Button text="Register" class="btn-info" url="register"/>
+                </>
+                
+              )}
+                
             </div>
         </nav>
     </>
