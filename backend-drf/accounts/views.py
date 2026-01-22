6b8088f83fa0d372b96c250c9cfa903459d189bd
Shravan017 <shravannaik027@gmail.com>
2026-01-22 22:58:53 +0530

    Simple jwt implimentation

--- backend-drf/accounts/views.py
+++ backend-drf/accounts/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from .serializers import UserSerializer
 from rest_framework import generics
 from django.contrib.auth.models import User
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 
 class RegisterView(generics.CreateAPIView):
@@ -10,3 +12,12 @@ class RegisterView(generics.CreateAPIView):
     serializer_class = UserSerializer
     #to make available for anyone , use permission classes and set it to "allowany"
     permission_classes = [AllowAny]
+    
+class ProtectedView(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request):
+        response = {
+            'status' : 'Request was permitted',
+        }
+        return Response(response)
