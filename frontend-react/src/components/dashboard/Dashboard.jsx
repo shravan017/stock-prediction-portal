5cbde5786cfc78daaa8329833115732aafed5f93
Shravan017 <shravannaik027@gmail.com>
2026-02-01 01:15:46 +0530

    Stockprediction API

--- frontend-react/src/components/dashboard/Dashboard.jsx
+++ frontend-react/src/components/dashboard/Dashboard.jsx
@@ -1,22 +1,44 @@
 import axios from 'axios'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axiosInstance from '../../axiosInstance'
 
 const Dashboard = () => {
-    
+    const [ticker, setTicker] = useState('')
     useEffect(() =>{
         const fetchProtectedData = async () =>{
             try{
                 const response = await axiosInstance.get('/protected-view/')
-                console.log('Success', response.data);
             } catch(error){
                 console.error('Erron in fetching data', error)
             }
         } 
         fetchProtectedData();
     },[])
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try{
+            const response = await axiosInstance.post('/predict/', {
+                ticker : ticker
+            })
+            console.log(response.data)
+        } catch(error){
+            console.error('Error in API request',error)
+        }
+    }
   return (
-    <div className='text-light container'>Dashboard</div>
+    <div className='container'>
+        <div className='row'>
+            <div className="col-md-6 mx-auto bg-light-dark p-5 rounded">
+                <h2 className='text-light mb-5'>Enter the Stock Ticker Here </h2>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" placeholder='Enter the Stock Ticker Here' className='form-control'
+                    onChange={(e) => setTicker(e.target.value)} required />
+                    <button type='submit' className='btn btn-info mt-4'>See Prediction</button>
+                </form>
+            </div>
+        </div>
+    </div>
   )
 } 
 
