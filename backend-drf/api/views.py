fa78bf228f1bf2d9eb29d08750c019a7ad9ea665
Shravan017 <shravannaik027@gmail.com>
2026-02-03 00:42:38 +0530

    plot the graph using matplotlib

--- backend-drf/api/views.py
+++ backend-drf/api/views.py
@@ -1,3 +1,5 @@
+import os
+from django.conf import settings
 from django.shortcuts import render
 from .serializers import StockPredictionSerializer
 from rest_framework.views import APIView
@@ -25,5 +27,23 @@ class StockPredictionAPIView(APIView):
                 return Response({ 'error' : 'No data found for the given ticker symbol.',
                                  'status': status.HTTP_404_NOT_FOUND
                                  })
+            df = df.reset_index()
+            
+            # Generate Basic Plot
+            plt.switch_backend('AGG') # Use a non-interactive backend (to save plots to files) AGG => Anti-Grain Geometry
+            plt.figure(figsize=(12,5))
+            plt.plot(df.Close, label = 'Closing Price')
+            plt.title(f'Closing Price of {ticker}')
+            plt.xlabel('Days')
+            plt.ylabel('Close Price')
+            plt.legend()
+            #save the plot to the file
+            plot_image_path = f"{ticker}_plot.png" #plotted image path
+            plot_img = os.path.join(settings.MEDIA_ROOT, plot_image_path) #server full path
+            plt.savefig(plot_img)
+            plt.close()
+            plot_img = settings.MEDIA_URL + plot_image_path
+            
+            
             return Response({'status':'Success'})
             
