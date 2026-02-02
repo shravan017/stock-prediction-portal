a46755eab89303a475728a6694cad6432f7b1ff8
Shravan017 <shravannaik027@gmail.com>
2026-02-03 00:42:38 +0530

    plot the graph using matplotlib

--- frontend-react/src/components/dashboard/Dashboard.jsx
+++ frontend-react/src/components/dashboard/Dashboard.jsx
@@ -1,9 +1,15 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import axiosInstance from '../../axiosInstance'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
 
 const Dashboard = () => {
     const [ticker, setTicker] = useState('')
+    const [error, setError] = useState()
+    const [loading, setLoading] = useState(false)
+    const [plot, setPlot] = useState()
+
     useEffect(() =>{
         const fetchProtectedData = async () =>{
             try{
@@ -17,13 +23,22 @@ const Dashboard = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        setLoading(true)
         try{
             const response = await axiosInstance.post('/predict/', {
                 ticker : ticker
             })
             console.log(response.data)
+            // set plot
+            const backendRoot = import.meta.env.VITE_BACKEND_ROOT
+            const plotURL = `${backendRoot}${response.data.plot_img}`
+            if(response.data.error){
+                setError(response.data.error)
+            }
         } catch(error){
             console.error('Error in API request',error)
+        } finally{
+            setLoading(false)
         }
     }
   return (
@@ -34,7 +49,12 @@ const Dashboard = () => {
                 <form onSubmit={handleSubmit}>
                     <input type="text" placeholder='Enter the Stock Ticker Here' className='form-control'
                     onChange={(e) => setTicker(e.target.value)} required />
-                    <button type='submit' className='btn btn-info mt-4'>See Prediction</button>
+                    <small>{error && <div className='text-danger mt-2'>{ error }</div>}</small>
+                    <button type='submit' className='btn btn-info mt-4'>
+                        {loading ? (
+                            <span><FontAwesomeIcon icon={faSpinner} spin />Please Wait...</span>
+                        ) : 'See Prediction'}
+                    </button>
                 </form>
             </div>
         </div>
